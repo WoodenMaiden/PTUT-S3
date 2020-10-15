e699ed62c203dca3a2cfde76316d5e6a4217dcf1
Parmenions <lilian.drets@etu.univ-amu.fr>
2020-10-15 08:49:08 +0200

    Commentaires sur GenerateurHtml

--- src/java/fr/univ_amu/DumbStages/GenerateurHtml.java
+++ src/java/fr/univ_amu/DumbStages/GenerateurHtml.java
@@ -10,11 +10,11 @@ public class GenerateurHtml {
     private String FinHtml;
     public String CodeHtml;
 
-    GenerateurHtml(String strEntree, String strSortie) throws IOException {
+    GenerateurHtml(String strEntree, String strSortie) throws IOException { //Constructeur
         this.Entree = new File(strEntree);
         this.Sortie = new File(strSortie);
         if (this.Sortie.createNewFile()) {
-            System.out.println("fichier créé !");
+            System.out.println("fichier créé !"); //Création de la première partie de l'html dans DebutHtml
             this.DebutHtml = new String("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
                     "<html><head>\n" +
                     "\t<meta content=\"text/html; charset=UTF-8\" http-equiv=\"content-type\">\n" +
@@ -33,14 +33,14 @@ public class GenerateurHtml {
                     "</div>\n" +
                     "\n" +
                     "<div style=\"text-align: center;\"><img style=\"width: 623px; height: 7px;\" alt=\"\" src=\"forum_fichiers/TraitCourant.gif\"></div>\n");
-            this.FinHtml = new String("</body></html>");
-            this.CodeHtml = new String ("<h1><span style=\"color: rgb(51, 102, 255);\"> Forum entreprise du");
+            this.FinHtml = new String("</body></html>"); //Création de la première partie de l'html dans FinHtml
+            this.CodeHtml = new String ("<h1><span style=\"color: rgb(51, 102, 255);\"> Forum entreprise du");//Création du reste du code dans CodeHtml
         }
     }
 
     public void setDate(String Date) {
         this.CodeHtml = CodeHtml + Date + " 23 janvier 2020</span></h1>";
-    }
+    }//Insert dans Codehtml l'Html affichant le titre de la page
 
     public void DebutTableau () {
         this.CodeHtml = CodeHtml + "\n" +
@@ -52,30 +52,19 @@ public class GenerateurHtml {
                 "        <th> Nom Entreprise </th>\n" +
                 "        <th> Repr&eacute;sentant Entreprise </th>\n" +
                 "\t    <th> Lien Web Entreprise </th>";
-    }
+    } //Insert dans CodeHtml le début du tableau en html
 
     public void FinTableau () {
         this.CodeHtml = this.CodeHtml + " </tbody></table>";
-    }
+    } //Insert dans CodeHtml la fin du tableau en html
 
     public void AjouterEntreprise (fr.univ_amu.DumbStages.donnees.Entreprise uneEntreprise){
         this.CodeHtml = this.CodeHtml + "    <tr>\n" +
-                "        <td> "+uneEntreprise.getNom_en()+"</td>";
-    }
-
-    public void AjouterRepresentants (fr.univ_amu.DumbStages.donnees.Entreprise uneEntreprise) {
-        this.CodeHtml = this.CodeHtml + "<td>";
+                "        <td> "+uneEntreprise.getNom_en()+"</td> <td>";
         for (String rep : uneEntreprise.getRepresentants())
             this.CodeHtml = this.CodeHtml + rep +"</br>";
-        this.CodeHtml = this.CodeHtml + "</td>";
-
-    }
-
-    public void AjouterUrl (fr.univ_amu.DumbStages.donnees.Entreprise uneEntreprise)
-    {
-        this.CodeHtml = this.CodeHtml+"<td><a href=\""+uneEntreprise.getUrl()+"\">"+uneEntreprise.getNom_en()+"</a></td>\n" +
-            "    </tr>";
-    }
+        this.CodeHtml = this.CodeHtml + "</td><td><a href=\""+uneEntreprise.getUrl()+"\">"+uneEntreprise.getNom_en()+"</a></td>\n</tr>";
+    }//Insert dans CodeHtml une ligne du tableau contenant le nom de l'entreprise, des représentants, ainsi que l'url de leur site
 
     public String getDebutHtml() {
         return DebutHtml;
@@ -85,20 +74,14 @@ public class GenerateurHtml {
         return FinHtml;
     }
 
-    public void EcritDansFichier() throws IOException {
-        FileWriter FichierEcriture = new FileWriter(this.Sortie);
-        FichierEcriture.write(this.CodeHtml);
-        FichierEcriture.close();
-    }
-
-    public void EcritDansFichierTest(String Texte) throws IOException {
+    public void EcritDansFichier(String Texte) throws IOException {
         FileWriter FichierEcriture = new FileWriter(this.Sortie);
         FichierEcriture.write(Texte);
         FichierEcriture.close();
-    }
+    } //Ecrit Texte dans le fichier Sortie /!\ Si utilisé deux fois sur le même fichier, le premier contenu sera remplacé par le deuxième
 
     public static void main(String[] args) throws IOException {
         GenerateurHtml gen = new GenerateurHtml("/amuhome/d19002305/Bureau/Logo.png", "/amuhome/d19002305/Bureau/masortie3.html");
-        gen.EcritDansFichierTest(gen.getDebutHtml() + "<h1>TEST</h1>" + gen.getFinHtml());
+        gen.EcritDansFichier(gen.getDebutHtml() + "<h1>TEST</h1>" + gen.getFinHtml());
     }
 }
