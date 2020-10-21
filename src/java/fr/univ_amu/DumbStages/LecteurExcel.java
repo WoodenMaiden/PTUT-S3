53e459dc986a1e0c8ea2791e7c227c9efe385e42
artho <artho340@gmail.com>
2020-10-21 12:22:12 +0200

    Implémentation d'un vecteur d'entreprises dans main()

--- src/java/fr/univ_amu/DumbStages/LecteurExcel.java
+++ src/java/fr/univ_amu/DumbStages/LecteurExcel.java
@@ -1,11 +1,11 @@
 package fr.univ_amu.DumbStages;
 
-
-import java.fr.univ_amu.DumbStages.donnees.Etudiant;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Vector;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet; //?
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -25,7 +25,7 @@ public class LecteurExcel {
         return monExcel2;
     }//?
 
-    public void AjouterEtudiant (Etudiant etudiant) { //Deuxième point du forum de stage
+    public void AjouterEtudiant (fr.univ_amu.DumbStages.donnees.Etudiant etudiant) { //Deuxième point du forum de stage
         this.monExcel2.createRow(this.monExcel2.getLastRowNum()+1).createCell(0).setCellValue(etudiant.getNom() +
                 etudiant.getPrenom() +", "+ etudiant.getGroupe());
     } //?
@@ -35,8 +35,8 @@ public class LecteurExcel {
                 entreprise.getNom_en());
     } //?
 
-
     public static void main(String[] args) {
+
         try {
             System.out.println("entrez le chemin absolu vers votre fichier :");
             Scanner entree = new Scanner(System.in);
@@ -44,14 +44,28 @@ public class LecteurExcel {
             LecteurExcel excelALire = new LecteurExcel(path);
             entree.close();
             System.out.println("fichier accédé !");
-        }
 
-        catch (Exception e) {
+            XSSFWorkbook fichier = excelALire.getFichier();
+
+            XSSFSheet mySheet = fichier.getSheetAt(0);
+
+            Vector<fr.univ_amu.DumbStages.donnees.Entreprise> MesEntreprises = new Vector<fr.univ_amu.DumbStages.donnees.Entreprise>();
+
+            for(Row row: mySheet)
+            {
+                if (row.getRowNum() != 0)
+                {
+                    String[] representants = new String[1];
+                    representants[0] = row.getCell(1).getStringCellValue();
+                    fr.univ_amu.DumbStages.donnees.Entreprise ent = new fr.univ_amu.DumbStages.donnees.Entreprise(row.getCell(0).getStringCellValue(), representants, row.getCell(2).getStringCellValue());
+                    MesEntreprises.add( ent );
+                    ent.show();
+                }
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             e.getStackTrace();
-        }
-
-        finally {
+        } finally {
             System.out.println("programme terminé");
         }
     }
